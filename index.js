28b17a2c456110b7b8bf5e7af5392d876ea68ee5
Tobias Butler <tobiasab@gmail.com>
2021-12-08 22:13:08 -0800

    set up for heroku

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Translate } = require('@google-cloud/translate').v2;
 
-const translate = new Translate({ key: 'AIzaSyC_df87kle4zCJqUPNswCGasUPscDYVuN8' });
+const translate = new Translate({ key: process.env.GOOGLE_TRANSLATE_API_KEY });
 
 const app = express();
 
 app.post('/api/translate', bodyParser.json(), async (req, res) => {
   const { text, options } = req.body;
   const [,result] = await translate.translate(text, options);
-  console.log('tarnslations', result);
   res.send(result);
 });
 
